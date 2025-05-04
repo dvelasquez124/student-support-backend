209713a3ad3e17c914d43b96687b1a3fc4d9d6f6
dvelasquez124 <89713276+dvelasquez124@users.noreply.github.com>
2025-05-04 17:45:52 -0400

    Re-add portal app to fix module import

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y#)j54hs!ojz(bm-5e6ec7r5s2y3nx4q!dd9z_l25v7&0=#0bp"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-y#)j54hs!ojz(bm-5e6ec7r5s2y3nx4q!dd9z_l25v7&0=#0bp")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
